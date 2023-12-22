706a6b8d71672f3173ace1f9461deb6ff801ab49
Avijit Singh <ajhyont@torontomu.ca>
2023-12-22 17:43:04 -0500

    night img added
    ok, so added the nightImg and it successfully relaces backgroundImg.
    NEXT: I'll b replacing night img with other imgs of the story succession and then, also, i'll readd the mascot.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = ()=>{
   useEffect(()=>{
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(backgroundRef.current,{
-      scrollTrigger:{ trigger: backgroundRef.current, start:'top+=888 top',end:'bottom bottom',scrub:1,markers:true },
+      scrollTrigger:{ trigger: backgroundRef.current, start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true },
       x:'-100vw',
       y:'0',
       ease:'none'
@@ -22,20 +22,19 @@ const Home = ()=>{
     gsap.fromTo(nightRef.current,
       { x:'100vw' },
       {
-        scrollTrigger: { trigger:nightRef.current,start:'top top',end:'bottom bottom',scrub:true, },
-        x:'100vw',
+        scrollTrigger: { trigger:nightRef.current,start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true },
+        x:'0',
       }
     );
   },[]);
   return(
-    <div style={{ position:'relative',height:'8888vh' }}>
+    <div style={{ position:'relative',height:'8888888vh' }}>
       <Navbar />
       <img ref={backgroundRef} src={backgroundImg} alt="Background" className="background-image"/>
         {/* // style={{ width:'100%',height:'95vh',objectFit:'cover',position:'absolute',top:'50px',left:0 }}  */}
       {/* <img src={m2} alt="Background" style={{ width:'100%',height:'95vh',objectFit:'cover',position:'absolute',top:0,left:0 }} /> */}
       {/* <img ref={mascotRef} src={mascotImg} alt="Mascot" style={{ width:'100px',position:'absolute' }} /> */}
-      <img ref={nightRef} src={night} alt="Background" 
-        style={{ width:'100%',height:'95vh',objectFit:'cover',position:'absolute',top:'50px',left:0}} />
+      <img ref={nightRef} src={night} alt="Background"  className="night-image"/>
     </div>
   );
 
