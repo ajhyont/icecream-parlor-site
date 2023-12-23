4b99c3893944c3e7025e8265bc089227a80a9b5e
Avijit Singh <ajhyont@torontomu.ca>
2023-12-23 03:23:08 -0500

    sun zoom out feature achieved
    So, I was able 2 update the animation so that it starts zoomed in to the sun completed and only after scrolling begins does the view zoom out and we see the entire 1st background.
    
    NEXT: i'll c if it's possible to have more control over the animations so that the reverse debocles can b resolved for a buttery smooth animation that i've seen in other websites. Also, I wud get a new delivery img. After that, readd the mascot into the whole shabang.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, {useEffect, useRef} from "react";
 import backgroundImg from './foundation_img_home_page.png';
 import mascotImg from './mascot_bike.png';
 import night from './image_cropped_16_9.png';
+import deliveryImg from './delivery.png';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Navbar from "../Navbar/Navbar";
@@ -11,30 +12,37 @@ const Home = ()=>{
   
   const backgroundRef = useRef(null);
   const nightRef = useRef(null);
+  const deliveryRef = useRef(null);
   useEffect(()=>{
     gsap.registerPlugin(ScrollTrigger);
-    gsap.to(backgroundRef.current,{
-      scrollTrigger:{ trigger: backgroundRef.current, start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true },
-      x:'-100vw',
-      y:'0',
-      ease:'none'
-    });    
-    gsap.fromTo(nightRef.current,
-      { x:'100vw' },
-      {
-        scrollTrigger: { trigger:nightRef.current,start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true },
-        x:'0',
-      }
-    );
+    gsap.set(backgroundRef.current, {scale:28, transformOrigin:'37% 1%'} );
+    const zoomOut_Tl=gsap.timeline( {scrollTrigger: {trigger:nightRef.current,start:'top+=10 top',end:'bottom bottom',scrub:1,markers:true},} );
+    zoomOut_Tl.to(backgroundRef.current, {scale:1,ease:'none'} );
+    const backgroundToNight_Tl = gsap.timeline( {scrollTrigger: {trigger:nightRef.current,start:'top+=888 top',end:'bottom bottom',scrub:1.2,markers:true},} );
+    backgroundToNight_Tl      
+      .to(backgroundRef.current, {x:'-66vw'} )
+      .fromTo(nightRef.current, {x:'100vw'},{x:'0'},);
+
+      const nightToDelivery_Tl = gsap.timeline( {scrollTrigger: {trigger:nightRef.current,start:'top+=1778 top',end:'bottom bottom',scrub:1.2,markers:true},} );
+      nightToDelivery_Tl
+        .to(nightRef.current, {x:'-66vw'} )
+        .fromTo(deliveryRef.current, {x:'100vw'},{x:'0'} );
+    // gsap.to(backgroundRef.current,{
+    //   scrollTrigger:{ trigger: backgroundRef.current, start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true },
+    //   x:'-100vw',y:'0',ease:'none'
+    // });    
+
+    // const nightTl = gsap.timeline({  scrollTrigger: {trigger:nightRef.current,start:'top+=888 top',end:'bottom bottom',scrub:3,markers:true},  });
+    // nightTl
+    //   .fromTo(nightRef.current, {x:'100vw'},{x:'0'} )
+    //   .to(nightRef.current, {x:'-100vw'} );
   },[]);
   return(
     <div style={{ position:'relative',height:'8888888vh' }}>
       <Navbar />
-      <img ref={backgroundRef} src={backgroundImg} alt="Background" className="background-image"/>
-        {/* // style={{ width:'100%',height:'95vh',objectFit:'cover',position:'absolute',top:'50px',left:0 }}  */}
-      {/* <img src={m2} alt="Background" style={{ width:'100%',height:'95vh',objectFit:'cover',position:'absolute',top:0,left:0 }} /> */}
-      {/* <img ref={mascotRef} src={mascotImg} alt="Mascot" style={{ width:'100px',position:'absolute' }} /> */}
-      <img ref={nightRef} src={night} alt="Background"  className="night-image"/>
+      <img ref={backgroundRef} src={backgroundImg} alt="Background" className="background-image"/>        
+      <img ref={nightRef} src={night} alt="night"  className="night-image"/>
+      <img ref={deliveryRef} src={deliveryImg} alt="deliveryImg"  className="delivery-image"/>
     </div>
   );
 
