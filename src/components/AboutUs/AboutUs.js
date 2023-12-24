49ad5176d07559961d8da98d7ea977311ebb0c33
Avijit Singh <ajhyont@torontomu.ca>
2023-12-24 01:21:58 -0500

    added extra imgs to aboutUs endppoint experiment
    So, I was able to add two mroe imgs to the aboutUs endpoint experiment. Pretty cool that it's working out just fine!
    NEXT UP: i'll try 2 give the horizontal slider effect the the imgs in the experiment. idea is basically 2 take this experiment as far as possible and combine this code with the root endpoint home page code.

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,15 +1,17 @@
 import React from "react";
+import './AboutUs.css';
 import Navbar from "../Navbar/Navbar";
 import backgroundImg from '../Home/foundation_img_home_page.png';
-import night from '../Home/delivery.png';
-import './AboutUs.css';
+import night from '../Home/image_cropped_16_9.png';
+import delivery from '../Home/delivery.png';
 
 const AboutUs=()=>{
   return(
-    <div style={{height:'888vh'}}>
+    <div style={{height:'8888vh'}}>
       <Navbar />
       <img src={backgroundImg} alt="backgroundImg" className="backgroundImg" />
-      {/* <img src={night} alt="night" className="night" /> */}
+      <img src={night} alt="night" className="night" />
+      <img src={delivery} alt="delivery" className="delivery" />
     </div>
   );
 };
