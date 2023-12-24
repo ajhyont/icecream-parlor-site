730799b4522c9d1f9b0dfdfa97c0a53e27c2b178
Avijit Singh <ajhyont@torontomu.ca>
2023-12-24 02:09:13 -0500

    horizontal scroll achieved
    ok, so i was able to get the night img to horizontally scroll into the viewpoint instead of the usual vertical entry.
    
    NEXT UP: i'll do the same thing for hte delivery img as well. Once that is done, i'll compare it with how I have it done currently in home endpoint and merge the two scripts together. Then, I can start playing with the last img which has more animations in it.

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect,useRef } from "react";
 import './AboutUs.css';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "../Navbar/Navbar";
 import backgroundImg from '../Home/foundation_img_home_page.png';
 import night from '../Home/image_cropped_16_9.png';
 import delivery from '../Home/delivery.png';
 
 const AboutUs=()=>{
+  const nightRef = useRef(null);
+  useEffect(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+    const nightImage = nightRef.current;
+    gsap.fromTo(nightRef.current,
+      {x:'100vw'},
+      {
+        x:'0',
+        scrollTrigger:{trigger:nightRef.current,start:"top+=800 bottom",end:"top top",scrub:1,markers:true}
+      });
+  },[]);
   return(
     <div style={{height:'8888vh'}}>
       <Navbar />
       <img src={backgroundImg} alt="backgroundImg" className="backgroundImg" />
-      <img src={night} alt="night" className="night" />
+      <img  ref={nightRef} src={night} alt="night" className="night" />
       <img src={delivery} alt="delivery" className="delivery" />
     </div>
   );
