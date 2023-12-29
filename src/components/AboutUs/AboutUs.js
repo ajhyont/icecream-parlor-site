d3e825ee8f890eefa5dca4871b5bb7fb20c683cc
Avijit Singh <ajhyont@torontomu.ca>
2023-12-29 01:49:44 -0500

    custom hook created for harmony of sticky effect and overflow-x: hidden!
    So, finally, i was able 2 find harmony b/t img stickiness at a certain position in the  window, and also remove x-axis overflow. this took the created of a custom js hook to do.
    
    NEXT UP: i'll create a global compoent out of this compnnet so that all other components can utilize this component, now under the GlobalComponents sub-folder in src main folder. Once this standrdization is successfully compoent, i'll apply it 2 the AboutUs experimental endpoint, and then  immidiately after, to the home webpage. Once that is done, lets c what excites me next!

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,78 +1,27 @@
 import React, { useEffect,useRef } from "react";
 import './AboutUs.css';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Navbar from "../Navbar/Navbar";
-import backgroundImg from '../Home/foundation_img_home_page.png';
-import mascotBike from '../Home/mascot_bike.png';
-import night from '../Home/image_cropped_16_9.png';
-import thirdImg from '../Home/thirdImg.png';
-import delivery from '../Home/delivery.png';
-import tailImg from '../Home/foundation_img_home_page.png';
-import storeImg from '../Home/icecream-parlor.png';
-import mascotLastImg from '../Home/mascot-last-img_inPixio3.png';
-import icecream_snow from '../Home/icecream_snow.png';
-import banner from '../Home/banner.png';
-
+import img from './test1.png';
 
 const AboutUs=()=>{
-
-  const backgroundRef = useRef(null);
-  const mascotBikeRef = useRef(null);
-  const nightRef = useRef(null);
-  const thirdImgRef = useRef(null);
-  const deliveryRef=useRef(null);
-  const tailRef=useRef(null);
-  const mascotRef=useRef(null);
-  const storeRef=useRef(null);
-  const icecream_snowRef=useRef(null);
+  
+  const imgRef = useRef(null);
   useEffect(()=>{
-    gsap.registerPlugin(ScrollTrigger);
-    // gsap.set(backgroundRef.current, {scale:24.5,transformOrigin:'36.81% 10.4%'} );
-    gsap.fromTo(backgroundRef.current,
-      { scale:24.5,transformOrigin:'36.81% 10.4%' },
-      { scale:1,ease:'none',scrollTrigger:{trigger:backgroundRef.current,start:'top top',end:'top center',scrub:1,markers:true} }
-    );
-    gsap.fromTo(mascotBikeRef.current,
-      {x:'-100vw'},{  x:'0',scrollTrigger:{trigger:mascotBikeRef.current,start:"top bottom",end:"top top",scrub:1,markers:true}  }
-    );
-    gsap.fromTo(nightRef.current,
-      {x:'100vw'},{  x:'0',scrollTrigger:{trigger:nightRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );
-    gsap.fromTo(thirdImgRef.current,
-      {x:'100vw'},{  x:'0',scrollTrigger:{trigger:thirdImgRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );
-    gsap.fromTo(mascotBikeRef.current,
-      {x:'0'},
-      { x:'100vw',scrollTrigger:{trigger:thirdImgRef.current,start:"top top-=300vh",end:"bottom+=100vh top",scrub:1,markers:true}  }
-    );
-    gsap.fromTo(deliveryRef.current,
-      {x:'100vw'},{  x:'0',scrollTrigger:{trigger:deliveryRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );
-    gsap.fromTo(tailRef.current,
-      {x:'100vw'},{  x:'0',scrollTrigger:{trigger:tailRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );
-    gsap.fromTo([storeRef.current,mascotRef.current],
-      {x:'-100vw'},{  x:'0',scrollTrigger:{trigger:storeRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );    
-    gsap.fromTo(icecream_snowRef.current,
-      {x:'100vw'},{  x:'0',scrollTrigger:{trigger:icecream_snowRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
-    );
-    
+    const handleScroll=()=>{
+      if(imgRef.current){
+        const stickyStart=imgRef.current.offsetTop-window.innerHeight*0.3;
+        const isSticky = window.scrollY > stickyStart;
+        imgRef.current.style.position = isSticky ? 'fixed' : 'absolute';        
+        // imgRef.current.style.top = isSticky ? '30vh' : 'initial';
+        imgRef.current.style.top = '30vh';
+      }
+    };
+    window.addEventListener('scroll',handleScroll);
+    return()=> window.removeEventListener('scroll',handleScroll);
   },[]);
   return(
-    <div>
-      <Navbar />
-      <img ref={backgroundRef} src={backgroundImg} alt="backgroundImg" className="backgroundImg" />
-      <img ref={mascotBikeRef} src={mascotBike} alt="mascotBike" className="mascotBike" />
-      <img ref={nightRef} src={night} alt="night" className="night" />
-      <img ref={thirdImgRef} src={thirdImg} alt="thirdImg" className="thirdImg" />
-      <img ref={deliveryRef} src={delivery} alt="delivery" className="delivery" />
-      <img ref={tailRef} src={tailImg} alt="tailImg" className="tail" />
-      <img ref={mascotRef} src={storeImg} alt="storeImg" className="store" />
-      <img ref={storeRef} src={mascotLastImg} alt="mascotLastImg" className="mascotLastImg" />
-      <img ref={icecream_snowRef} src={icecream_snow} alt="icecream_snow" className="icecream_snow" />
-      <a href="/icecream-parlor-site/contact"><img src={banner} alt="banner" className="banner" /></a>
+    <div className="main">      
+      <img ref={imgRef} src={img} alt="img" className="imgs" />      
+      <div className="wide-content">Waheguruji!</div>      
     </div>
   );
 
