5150a6e1dd0db4c29830bd62d2e64ab801563360
Avijit Singh <ajhyont@torontomu.ca>
2023-12-29 04:33:51 -0500

    navbar equiped with useSticky custom hook!
    ok, so status is that i've started recreating the home webpage in the aboutus endpoint with the new useSticky custom hook. slowly but surely im gonna uncomment each set of code stmts and get each img working and fitted one by one, until all is working. This wud b easy and maybe i'll come up with a kool new concept while doing it as well.thos r the NEXT STEPS.
    after that, i can think about replacing the aboutus code about to home webpage. Then, we'll c.

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,23 +1,83 @@
 import React, { useEffect,useRef } from "react";
-import useSticky from "../../GlobalComponents/useSticky";
 import './AboutUs.css';
-import img from './test1.png';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Navbar from "../Navbar/Navbar";
 import backgroundImg from '../Home/foundation_img_home_page.png';
+import mascotBike from '../Home/mascot_bike.png';
+import night from '../Home/image_cropped_16_9.png';
+import thirdImg from '../Home/thirdImg.png';
+import delivery from '../Home/delivery.png';
+import tailImg from '../Home/foundation_img_home_page.png';
+import storeImg from '../Home/icecream-parlor.png';
+import mascotLastImg from '../Home/mascot-last-img_inPixio3.png';
+import icecream_snow from '../Home/icecream_snow.png';
+import banner from '../Home/banner.png';
+import useSticky from "../../GlobalComponents/useSticky";
 
-const AboutUs=()=>{
-  
-  const imgRef=useRef(null);
-  const bkRef=useRef(null);
-  useSticky(0.01,'0vh',imgRef);
-  useSticky(0.01,'20vh',bkRef);
+
+const Home=()=>{
+
+  const backgroundRef = useRef(null);
+  const mascotBikeRef = useRef(null);
+  const nightRef = useRef(null);
+  const thirdImgRef = useRef(null);
+  const deliveryRef=useRef(null);
+  const tailRef=useRef(null);
+  const mascotRef=useRef(null);
+  const storeRef=useRef(null);
+  const icecream_snowRef=useRef(null);
+  useSticky([backgroundRef,nightRef,thirdImgRef,deliveryRef,tailRef],0.0,'1vh');
+
+  useEffect(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+    // gsap.fromTo(backgroundRef.current,
+    //   { scale:24.5,transformOrigin:'36.81% 10.4%' },
+    //   { scale:1,ease:'none',scrollTrigger:{trigger:backgroundRef.current,start:'top top',end:'top center',scrub:1,markers:true} }
+    // );
+    // gsap.fromTo(mascotBikeRef.current,
+    //   {x:'-100vw'},{  x:'0',scrollTrigger:{trigger:mascotBikeRef.current,start:"top bottom",end:"top top",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo(nightRef.current,
+    //   {x:'100vw'},{  x:'0',scrollTrigger:{trigger:nightRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo(thirdImgRef.current,
+    //   {x:'100vw'},{  x:'0',scrollTrigger:{trigger:thirdImgRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo(mascotBikeRef.current,
+    //   {x:'0'},
+    //   { x:'100vw',scrollTrigger:{trigger:thirdImgRef.current,start:"top top-=300vh",end:"bottom+=100vh top",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo(deliveryRef.current,
+    //   {x:'100vw'},{  x:'0',scrollTrigger:{trigger:deliveryRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo(tailRef.current,
+    //   {x:'100vw'},{  x:'0',scrollTrigger:{trigger:tailRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );
+    // gsap.fromTo([storeRef.current,mascotRef.current],
+    //   {x:'-100vw'},{  x:'0',scrollTrigger:{trigger:storeRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );    
+    // gsap.fromTo(icecream_snowRef.current,
+    //   {x:'100vw'},{  x:'0',scrollTrigger:{trigger:icecream_snowRef.current,start:"top top",end:"top top-=100vh",scrub:1,markers:true}  }
+    // );
+    
+  },[]);
   return(
-    <div className="main">      
-      <img ref={imgRef} src={img} alt="img" className="imgs" />    
-      <img ref={bkRef} src={backgroundImg} alt="backgroundImg" className="backgroundImg" />
-      <div className="wide-content">Waheguruji!</div>      
+    <div className="main-container" style={{height:'500vh'}}>
+      <Navbar />      
+      {/* <img ref={backgroundRef} src={backgroundImg} alt="backgroundImg" className="backgroundImg" />
+      <img ref={mascotBikeRef} src={mascotBike} alt="mascotBike" className="mascotBike" />
+      <img ref={nightRef} src={night} alt="night" className="night" />
+      <img ref={thirdImgRef} src={thirdImg} alt="thirdImg" className="thirdImg" />
+      <img ref={deliveryRef} src={delivery} alt="delivery" className="delivery" />
+      <img ref={tailRef} src={tailImg} alt="tailImg" className="tail" />
+      <img ref={mascotRef} src={storeImg} alt="storeImg" className="store" />
+      <img ref={storeRef} src={mascotLastImg} alt="mascotLastImg" className="mascotLastImg" />
+      <img ref={icecream_snowRef} src={icecream_snow} alt="icecream_snow" className="icecream_snow" />
+      <a href="/icecream-parlor-site/contact"><img src={banner} alt="banner" className="banner" /></a> */}
     </div>
   );
 
 };
 
-export default AboutUs;
+export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
+import useSticky from "../../GlobalComponents/useSticky";
 
 function Navbar(){
   const linkStyle={ margin:'0 1em', textDecoration:'none', color:'inherit' };
+  const navbarRef=useRef(null);
+  useSticky([navbarRef],0.0,'1vh');
   return(
-    <nav className="navbar">      
+    <nav ref={navbarRef} className="navbar">      
         <Link to="/icecream-parlor-site/" style={linkStyle}>Home</Link>
         <Link to="/icecream-parlor-site/aboutUs" style={linkStyle}>About Us</Link>
         <Link to="/icecream-parlor-site/contact" style={linkStyle}>Contact</Link>      
